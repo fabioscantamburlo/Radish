11c659d0632307bb7f04020887199ffd9ca54873
fabioscantamburlo <fabioscantamburlo1@gmail.com>
2025-10-24 17:29:18 +0200

    Added radish statement and wip linked lists

--- src/rlinkedlists.jl
+++ src/rlinkedlists.jl
@@ -3,10 +3,29 @@ using .Radish
 using Dates
 
 
-mutable struct LinkedListElement{T}
-    data::T
-    next::Union{LinkedListElement{T}, Nothing}
+# DoubleLinkedList element for RadishDb
+# The idea is to have a double linked list in which the first element has prev nothing
+# The last element has next nothing so it's gonna be easy to going up-down the the list.
+
+mutable struct DLinkedStartEnd{T}
+    top::Union{DLinkedListElement{T}, Nothing}
+    bot::Union{DLinkedListElement{T}, Nothing}
 end
+mutable struct DLinkedListElement{T}
+    data::T
+    next::Union{DLinkedListElement{T}, Nothing}
+    prev::Union{DLinkedListElement{T}, Nothing}
+end
+
+
+# LPUSH adds a new element to the head of a list; RPUSH adds to the tail.
+# LPOP removes and returns an element from the head of a list; RPOP does the same but from the tails of a list.
+# LLEN returns the length of a list.
+# LMOVE atomically moves elements from one list to another.
+# LRANGE extracts a range of elements from a list.
+# LTRIM reduces a list to the specified range of elements.
+
+
 
 const LL_PALETTE = Dict{String, Tuple}(
     "S_GET" => (sget, rget_or_expire!),
