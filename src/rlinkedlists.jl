ce1433b700f25def1badfa40b1779b412d70bd31
fabioscantamburlo <fabioscantamburlo1@gmail.com>
2025-10-29 16:55:30 +0100

    linked list 1st attempt

--- src/rlinkedlists.jl
+++ src/rlinkedlists.jl
@@ -12,13 +12,47 @@ mutable struct DLinkedListElement{T}
     next::Union{DLinkedListElement{T}, Nothing}
     prev::Union{DLinkedListElement{T}, Nothing}
 end
+
 mutable struct DLinkedStartEnd{T}
     head::Union{DLinkedListElement{T}, Nothing}
     tail::Union{DLinkedListElement{T}, Nothing}
     len::Int
+    
     DLinkedStartEnd{T}() where T = new{T}(nothing, nothing, 0)
+    DLinkedStartEnd{T}(h, t, l) where T = new{T}(h, t, l)
 end
 
+
+function DLinkedStartEnd(value::T) where T
+    new_element = DLinkedListElement(value, nothing, nothing)
+    return DLinkedStartEnd{T}(new_element, new_element, 1)
+end
+
+function Base.push!(list::DLinkedStartEnd{T}, value::T) where T
+    new_element = DLinkedListElement(value, nothing, nothing)
+
+    if list.len == 0
+        list.head = new_element
+        list.tail = new_element
+    else
+        new_element.prev = list.tail 
+        list.tail.next = new_element 
+        list.tail = new_element
+    end
+
+    list.len += 1
+    return list
+end
+
+# Traversal function (now part of the module)
+function traverse_linked_list_backward(list::DLinkedStartEnd)
+    println("Traversing backward:")
+    j = list.tail
+    while j !== nothing
+        println(j.data)
+        j = j.prev
+    end
+end
 
 
 # LPUSH adds a new element to the head of a list; RPUSH adds to the tail.
